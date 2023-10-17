92a060c9f3264ab2ebdcafc534d4c7892476ed64
Manthan Kuber <manthankuber873@gmail.com>
2023-10-17 11:39:55 +0530

    feat: make requests to container and store in o/p

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import RandomNumbers from "../get-random-numbers.json";
 
 type resType = {
   x: string | null;
@@ -8,48 +9,64 @@ type resType = {
   error: boolean;
 };
 
+const randomNumsArr = RandomNumbers.numbers;
+
 function App() {
-  const [value, setValue] = useState("");
-  const [result, setResult] = useState<string | null>("");
-  const [isErr, setIsErr] = useState(false);
+  const [result, setResult] = useState([""]);
+  const [isFileFilled, setIsFileFilled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (result.length === randomNumsArr.length) setIsFileFilled(true);
+  }, [result]);
 
   const getPi = async () => {
-    let urlObj = {
-      url: "http://localhost:4000",
-      file: "part_1.txt",
-    };
-    if (parseInt(value) >= 45215800000000) {
-      urlObj.url = "http://localhost:4001";
-      urlObj.file = "part_2.txt";
+    setIsLoading(true);
+    for (let i = 0; i < 5; i++) {
+      let urlObj = {
+        url: "http://localhost:4000",
+        file: "part_1.txt",
+      };
+      if (randomNumsArr[i] >= 45215800000000) {
+        urlObj.url = "http://localhost:4001";
+        urlObj.file = "part_2.txt";
+      }
+      try {
+        const res = await axios(
+          `${urlObj.url}/compute-pi/${randomNumsArr[i]}/${urlObj.file}`
+        );
+        const { x, pi_x, error }: resType = res.data;
+        setResult((prev) => [...prev, pi_x!, "\n"]);
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+      }
     }
-    try {
-      const res = await axios(
-        `${urlObj.url}/compute-pi/${value}/${urlObj.file}`
-      );
-      const { x, pi_x, error }: resType = res.data;
-      setResult(pi_x);
-      setIsErr(error);
-    } catch (e) {
-      console.log(e);
-      setIsErr(true);
-    }
+    setIsLoading(false);
+  };
+
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    // setResult(result.slice(1));
+    const file = new Blob(result!, { type: "text/plain" });
+    link.href = URL.createObjectURL(file);
+    link.download = "output.txt";
+    link.click();
+    setResult([""])
+    URL.revokeObjectURL(link.href);
   };
 
   return (
     <>
       <h1>Compute Pi</h1>
       <div className="card">
-        <p>Enter value of x</p>
-        <div className="input-wrapper" >
-          <input
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          />
+        <div className="input-wrapper">
           <button onClick={getPi}>Compute</button>
+          {isLoading && <span>Loading ...</span>}
+          {!isLoading && (
+            <button onClick={downloadFile}>Download Text File</button>
+          )}
         </div>
-        {result && <p>The value of pi(x) is: {result}</p>}
       </div>
     </>
   );
